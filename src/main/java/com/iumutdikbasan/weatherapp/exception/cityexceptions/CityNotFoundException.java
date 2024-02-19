f8ca12326e82ecec661147710b17f7ac4d1af0e0
iumutdikbasan <iumutdikbasan@gmail.com>
2024-02-19 16:16:40 +0300

    First kafka configuration.

--- src/main/java/com/iumutdikbasan/weatherapp/exception/cityexceptions/CityNotFoundException.java
+++ src/main/java/com/iumutdikbasan/weatherapp/exception/cityexceptions/CityNotFoundException.java
@@ -1,12 +1,5 @@
 package com.iumutdikbasan.weatherapp.exception.cityexceptions;
 
-import com.iumutdikbasan.weatherapp.dto.city.errorresponse.CityExceptionResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.LocalDateTime;
 
 /*
     Uygulama üzerinden istemci tarafına iletilecek olan
@@ -16,7 +9,7 @@ import java.time.LocalDateTime;
     farklı HTTP statü kodlarını kullanarak özelleştirebilir
     veya ihtiyaçlar doğrultusunda değiştirdiğimiz cevapları istemci tarafına iletebiliriz.
 */
-@RestControllerAdvice
+
 public class CityNotFoundException extends RuntimeException {
 
     public CityNotFoundException(String message) {
--- src/main/java/com/iumutdikbasan/weatherapp/exception/exceptionhandler/CityExceptionsHandler.java
+++ src/main/java/com/iumutdikbasan/weatherapp/exception/exceptionhandler/CityExceptionsHandler.java
@@ -11,6 +11,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+
+
+/*
+    Uygulama üzerinden istemci tarafına iletilecek olan
+    exceptionları yöneterek global exception handling yapmamıza olanak sağlar.
+    Bu anotasyonu kullarak uygulamamızın herhangi bir noktasından fırlatılan
+    ve Controller’lar üzerinden istemciye iletilen hataların tiplerini dönüştürebilir,
+    farklı HTTP statü kodlarını kullanarak özelleştirebilir
+    veya ihtiyaçlar doğrultusunda değiştirdiğimiz cevapları istemci tarafına iletebiliriz.
+*/
+
 @RestControllerAdvice
 public class CityExceptionsHandler {
 
